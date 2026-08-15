58b79e665bb76b1b018ecaad099880119beb09dc
cloudyana <cloudina@gmail.com>
2026-08-14 23:58:51 -0400

    Add Application Load Balancer and target group

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -180,3 +180,54 @@ resource "aws_security_group" "app" {
     Environment = "dev"
   }
 }
+
+
+resource "aws_lb" "app" {
+  name               = "aws-cloud-portfolio-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  tags = {
+    Name        = "aws-cloud-portfolio-alb"
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name     = "aws-cloud-portfolio-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name        = "aws-cloud-portfolio-tg"
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
