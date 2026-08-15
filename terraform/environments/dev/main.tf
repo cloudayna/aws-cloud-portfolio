f9225108c8e2b3b4bfca3f32261cfb5fb1ea38f1
cloudyana <cloudina@gmail.com>
2026-08-14 22:30:52 -0400

    Add initial Terraform AWS VPC configuration

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "aws-cloud-portfolio-vpc"
+    Environment = "dev"
+  }
+}
