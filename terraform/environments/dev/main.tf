ad11f8401c5b62df3926ce96b7d4772ed4e6e974
cloudyana <cloudina@gmail.com>
2026-08-15 15:50:05 -0400

    Add CPU target tracking autoscaling policy

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -354,3 +354,17 @@ resource "aws_autoscaling_group" "app" {
 }
 
 
+resource "aws_autoscaling_policy" "cpu_target" {
+  name                   = "aws-cloud-portfolio-cpu-scaling"
+  autoscaling_group_name = aws_autoscaling_group.app.name
+
+  policy_type = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
